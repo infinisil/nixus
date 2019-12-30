f15e9008289cc66b42ebd3997c64bc7367390d5d
Silvan Mosberger <contact@infinisil.com>
2019-12-30 18:45:15 +0100

    Clean up file structure some more and add option descriptions

--- default.nix
+++ default.nix
@@ -1,86 +1,8 @@
-let
+conf: let
   # TODO: What nixpkgs should be used here?
-  pkgs = import <nixpkgs> {};
-  inherit (pkgs) lib;
-  inherit (lib) types;
-
-  extraConfig = { lib, ... }: {
-    systemd.services.sshd.stopIfChanged = lib.mkForce true;
-  };
-
-  switch = pkgs.runCommandNoCC "switch" {
-    # TODO: Make NixOS module for this
-    switchTimeout = 120;
-    successTimeout = 10;
-  } ''
-    mkdir -p $out/bin
-    substituteAll ${scripts/switch} $out/bin/switch
-    chmod +x $out/bin/switch
-  '';
-
-  optionsModule = { config, ... }: {
-    options.machines = lib.mkOption {
-      type = types.attrsOf (types.submodule ({ name, config, ... }: {
-        options.host = lib.mkOption {
-          type = types.str;
-        };
-
-        options.nixpkgs = lib.mkOption {
-          type = types.path;
-          description = "nixpkgs to use";
-        };
-
-        options.configuration = lib.mkOption {
-          # TODO: Specify for merging and inter-machine abstractions
-          type = types.unspecified;
-        };
-
-        options.deployScript = lib.mkOption {
-          type = types.package;
-          readOnly = true;
-        };
-
-        config.deployScript = pkgs.runCommandNoCC "deploy-${name}" {
-          inherit (config) host;
-          inherit switch;
-          hostname = name;
-          # TODO: Pass lib as specialArgs
-          systembuild = (import (config.nixpkgs + "/nixos") {
-            configuration = {
-              imports = [
-                config.configuration
-                extraConfig
-              ];
-            };
-          }).config.system.build.toplevel;
-        } ''
-          mkdir -p $out/bin
-          substituteAll ${scripts/deploy} $out/bin/deploy
-          chmod +x $out/bin/deploy
-        '';
-      }));
-      description = "machines";
-    };
-
-    options.deployScript = lib.mkOption {
-      type = types.package;
-      readOnly = true;
-    };
-
-    config.deployScript = pkgs.writeScript "deploy" ''
-      #!${pkgs.runtimeShell}
-      ${lib.concatMapStrings (machine: ''
-        ${machine.deployScript}/bin/deploy &
-      '') (lib.attrValues config.machines)}
-      wait
-    '';
-  };
-
-in conf:
-let
-  result = lib.evalModules {
+  result = (import <nixpkgs/lib>).evalModules {
     modules = [
-      optionsModule
+      ./options.nix
       conf
     ];
   };
---
+++ options.nix
@@ -0,0 +1,114 @@
+{ config, lib, ... }:
+let
+  # TODO: How to make this pure?
+  pkgs = import <nixpkgs> {};
+  inherit (lib) types;
+
+  switch = pkgs.runCommandNoCC "switch" {
+    # TODO: Make NixOS module for this
+    switchTimeout = 120;
+    successTimeout = 10;
+  } ''
+    mkdir -p $out/bin
+    substituteAll ${scripts/switch} $out/bin/switch
+    chmod +x $out/bin/switch
+  '';
+
+  extraConfig = { lib, ... }: {
+    systemd.services.sshd.stopIfChanged = lib.mkForce true;
+  };
+
+  machineType = { name, config, ... }: {
+
+    options = {
+      # TODO: What about different ssh ports? Some access abstraction perhaps?
+      host = lib.mkOption {
+        type = lib.types.str;
+        example = "root@172.18.67.46";
+        description = ''
+          How to reach the host via ssh.
+        '';
+      };
+
+      nixpkgs = lib.mkOption {
+        type = lib.types.path;
+        example = lib.literalExample ''
+          fetchTarball {
+            url = "https://github.com/NixOS/nixpkgs/tarball/a06925d8c608d7ba1d4297dc996c187c37c6b7e9";
+            sha256 = "0xy6rimd300j5bdqmzizs6l71x1n06pfimbim1952fyjk8a3q4pr";
+          }
+        '';
+        description = ''
+          The path to the nixpkgs version to use for this host.
+        '';
+      };
+
+      configuration = lib.mkOption {
+        # TODO: Specify for merging and inter-machine abstractions and allowing access to configuration values
+        type = lib.types.unspecified;
+        example = lib.literalExample ''
+          {
+            imports = [ ./hardware-configuration.nix ];
+            boot.loader.grub.device = "/dev/sda";
+            networking.hostName = "test";
+          }
+        '';
+        description = ''
+          The NixOS configuration for this host.
+        '';
+      };
+
+      deployScript = lib.mkOption {
+        type = lib.types.package;
+        readOnly = true;
+        description = ''
+          The path to the script to deploy all hosts.
+        '';
+      };
+    };
+
+    config = let
+      system = (import (config.nixpkgs + "/nixos") {
+        configuration = {
+          imports = [
+            config.configuration
+            extraConfig
+          ];
+        };
+      }).config.system.build.toplevel;
+    in {
+      deployScript = pkgs.runCommandNoCC "deploy-${name}" {
+        hostname = name;
+        inherit (config) host;
+        inherit switch;
+        systembuild = system;
+      } ''
+        mkdir -p $out/bin
+        substituteAll ${scripts/deploy} $out/bin/deploy
+        chmod +x $out/bin/deploy
+      '';
+    };
+  };
+
+in {
+  options = {
+    machines = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.submodule machineType);
+      description = "machines";
+    };
+
+    deployScript = lib.mkOption {
+      type = lib.types.package;
+      readOnly = true;
+    };
+  };
+
+  # TODO: What about requiring either all machines to succeed or all get rolled back?
+  config.deployScript = pkgs.writeScript "deploy" ''
+    #!${pkgs.runtimeShell}
+    ${lib.concatMapStrings (machine: ''
+      ${machine.deployScript}/bin/deploy &
+    '') (lib.attrValues config.machines)}
+    wait
+  '';
+}
