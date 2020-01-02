f2d92534c03cf0a31035d5226b58c2a74a7b4f0b
Silvan Mosberger <contact@infinisil.com>
2020-01-02 20:42:31 +0100

    Use upstream nixpkgs version

--- default.nix
+++ default.nix
@@ -1,6 +1,11 @@
 conf: let
-  # TODO: What nixpkgs should be used here?
-  result = (import nixpkgs/lib).evalModules {
+  nixpkgs = fetchTarball {
+    # Needs to include changes from https://github.com/NixOS/nixpkgs/pull/75031
+    url = "https://github.com/NixOS/nixpkgs/tarball/cdf79db19d6ce1f305acebd86dabd58edb42e7c0";
+    sha256 = "0m7ib1qdyymsg3kq10pcy0239gacvrc605vxgvwghyp6axib24ff";
+  };
+
+  result = (import (nixpkgs + "/lib")).evalModules {
     modules = [
       ./options.nix
       conf
