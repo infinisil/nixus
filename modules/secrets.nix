bf9611f10cb9fa7f39fc906371ddfca62ac79d37
Silvan Mosberger <contact@infinisil.com>
2020-06-01 04:57:05 +0200

    Improved secret handling for permissions and structure
    - Secrets now belong to either a user or a group (but never both)
    - This allows per-user/$user/$secret directories with permissions to
      only allow that user to know which secrets belong to them
    - Secrets are now also stored in /var/lib/nixus-secrets (for migration,
      manually remove /var/keys)
    - Secrets are now switched more atomically
    
    Update secret permissions docs

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -9,26 +9,35 @@ let
 
   # Note: This is persisted
   # Note: NixOS by default adds /run/keys as a ramfs with 750 permissions and group config.ids.gids.key
-  keyDirectory = "/var/keys";
+  keyDirectory = "/var/lib/nixus-secrets";
 
-  secretType = pkgs: { name, ... }: {
+  secretType = pkgs: { name, config, ... }: {
     options = {
       file = lib.mkOption {
         type = types.path;
-        apply = indirectSecret pkgs name;
+        apply = indirectSecret pkgs config name;
       };
       user = lib.mkOption {
-        type = types.str;
-        default = "root";
+        type = types.nullOr types.str;
+        default = null;
         description = ''
-          The owner of the file
+          The owning user of the secret. If this is set, only that user can
+          access the secret. Mutually exclusive with setting a group.
+        '';
+      };
+      group = lib.mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        description = ''
+          The owning group of the secret. If this is set, only that group can
+          access the secret. Mutually exclusive with setting a user.
         '';
       };
     };
   };
 
   # Takes a file path and turns it into a derivation
-  indirectSecret = pkgs: name: file: pkgs.runCommandNoCC "secret-${name}" {
+  indirectSecret = pkgs: config: name: file: pkgs.runCommandNoCC "secret-${name}" {
     # To find out which file to copy. toString to not import the secret into
     # the store
     file = toString file;
@@ -39,9 +48,18 @@ let
     # TODO: Switch to `builtins.hashFile "sha512" value`
     # which requires Nix 2.3. The readFile way can cause an error when it
     # contains null bytes
-  } ''
-    ln -s ${keyDirectory}/${name} $out
-  '';
+  } (
+    let
+      validSecret = (config.user == null) != (config.group == null);
+      subdir = if config.user != null
+        then "per-user/${config.user}"
+        else "per-group/${config.group}";
+      target = if validSecret
+        then "${keyDirectory}/active/${subdir}/${name}"
+        else throw "nixus: secret.${name} needs to either have a user or a group set, but not both";
+    in ''
+      ln -s ${lib.escapeShellArg target} "$out"
+    '');
 
   # Intersects the closure of a system with a set of secrets
   requiredSecrets = pkgs: { system, secrets }: pkgs.stdenv.mkDerivation {
@@ -56,7 +74,7 @@ let
       path = value.file;
       source = value.file.file;
       hash = value.file.secretHash;
-      inherit (value) user;
+      inherit (value) user group;
     }) secrets;
 
     PATH = lib.makeBinPath [pkgs.buildPackages.jq];
@@ -89,57 +107,96 @@ in {
         };
       };
 
+      # These scripts are intentionally not conditioned on secrets being defined
+      # This is such that if a user enabled and disables secrets, they won't
+      # stick around
       config =
         let
           includedSecrets = requiredSecrets pkgs {
             system = config.configuration.system.build.toplevel;
             secrets = config.configuration.secrets;
           };
+
         in {
 
-        # We can't use tmpfiles for this because we only know what secrets are included at build-time
-        # TODO: Use deploy script for this instead, because activation scripts are retained and can't be updated later
-        configuration.system.activationScripts.secret-owners = lib.stringAfter [ "users" "groups" ] ''
-          if [ -f /run/included-secrets ]; then
+        /*
+
+        Secret structure:
+        /var/lib/nixus-secrets/active  root:root    0755   # Directory containing all active persisted secrets and data needed to support it
+          |
+          + included-secrets           root:root    0750   # A file containing line-delimited json values describing all present secrets
+          |
+          + per-user                   root:root    0755   # A directory containing all secrets owned by users
+          | |
+          | + <user>                   <user>:root  0500   # A directory containing all secrets owned by <user>
+          |   |                                            # Permissions are as restrictive as possible, some programs like ssh require this
+          |   |
+          |   + <name>                 <user>:root  0400   # A file containing the secret <name>
+          |
+          + per-group                  root:root    0755   # A directory containing all secrets owned by groups
+            |
+            + <group>                  root:<group> 0050   # A directory containing all secrets owned by <group>
+              |
+              + <name>                 root:<group> 0040   # A file containing the secret <name>
+
+        /var/lib/nixus-secrets/pending root:root    0755   # The same structure as /active, but this is only used during deployment to make it more atomic and simple to remove unneeded ones later
+                                                           # The only difference here is that no owners are set yet, since we can't yet know uid and gid
+        */
+
+        # Configures owners for secrets. This needs to happen during activation since only then the users/groups even exist
+        # And this can't be done with tmpfiles.d because that would be part of the system closure, which is evaluated to even know which secrets to include
+        # FIXME: This only works with rollbacks because nixus rolls back by essentially redeploying. So this will probably not work with e.g. grub rollbacks
+        configuration.system.activationScripts.activate-secrets = lib.stringAfter [ "users" "groups" ] ''
+          if [[ -d ${keyDirectory}/pending ]]; then
             while read -r json; do
               name=$(echo "$json" | ${pkgs.jq}/bin/jq -r '.name')
               user=$(echo "$json" | ${pkgs.jq}/bin/jq -r '.user')
-              chown -v "$user": "${keyDirectory}/$name"
-            done < /run/included-secrets
+              group=$(echo "$json" | ${pkgs.jq}/bin/jq -r '.group')
 
-            rm /run/included-secrets
+              # If this is a per-user secret
+              if [[ "$user" != null ]]; then
+                chown -v -R "$user":root "${keyDirectory}/pending/per-user/$user"
+              else
+                chown -v -R root:"$group" "${keyDirectory}/pending/per-group/$group"
+              fi
+            done < ${keyDirectory}/pending/included-secrets
+
+            # TOOD: Do this atomically
+            if [[ -d ${keyDirectory}/active ]]; then
+              rm -r ${keyDirectory}/active
+            fi
+            mv -v ${keyDirectory}/pending ${keyDirectory}/active
           fi
         '';
 
         deployScriptPhases.secrets = lib.dag.entryBefore ["switch"] ''
           echo "Copying secrets..." >&2
 
-          ssh "$HOST" sudo mkdir -v -p -m 755 ${keyDirectory}
-          rsync --rsync-path="sudo rsync" "${includedSecrets}" "$HOST":/run/included-secrets
+          ssh "$HOST" sudo mkdir -p -m 755 ${keyDirectory}/pending/per-{user,group}
+          # TODO: I don't think this works if rsync isn't on the remote's shell.
+          # We really just need a single binary we can execute on the remote, like the switch script
+          rsync --perms --chmod=750 --chown "root:root" --rsync-path="set -o noglob; sudo rsync" "${includedSecrets}" "$HOST:${keyDirectory}/pending/included-secrets"
 
           while read -r json; do
             name=$(echo "$json" | jq -r '.name')
             source=$(echo "$json" | jq -r '.source')
-            echo "Copying secret '$name'" >&2
-            rsync --perms --chmod=440 --rsync-path="sudo rsync" "$source" "$HOST":${keyDirectory}/"$name"
-          done < ${includedSecrets}
-        '';
+            user=$(echo "$json" | jq -r '.user')
+            group=$(echo "$json" | jq -r '.group')
 
-        deployScriptPhases.remove-secrets = lib.dag.entryAfter ["switch"] ''
-          if [ "$status" = success ]; then
-            mapfile -t requiredNames < <(jq -r '.name' ${includedSecrets})
+            echo "Copying secret $name..." >&2
 
-            echo "Removing secrets that aren't needed anymore..." >&2
-            while IFS= read -r -d "" secret; do
-              for name in "''${requiredNames[@]}"; do
-                if [ "${keyDirectory}/$name" = "$secret" ]; then
-                  continue 2
-                fi
-              done
-              echo "Removing secret '$secret'"
-              ssh "$HOST" sudo rm "$secret"
-            done < <(ssh "$HOST" sudo find ${keyDirectory} -type f -print0)
-          fi
+            # If this is a per-user secret
+            if [[ "$user" != null ]]; then
+              # The -n is very important for ssh to not swallow stdin!
+              ssh -n "$HOST" sudo mkdir -p -m 500 "${keyDirectory}/pending/per-user/$user"
+              rsync --perms --chmod=400 --rsync-path="sudo rsync" "$source" "$HOST:${keyDirectory}/pending/per-user/$user/$name"
+            else
+              ssh -n "$HOST" sudo mkdir -p -m 050 "${keyDirectory}/pending/per-group/$group"
+              rsync --perms --chmod=040 --rsync-path="sudo rsync" "$source" "$HOST:${keyDirectory}/pending/per-group/$group/$name"
+            fi
+          done < "${includedSecrets}"
+
+          echo "Finished copying secrets" >&2
         '';
       };
     });
