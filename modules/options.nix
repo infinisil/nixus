621badd51e3e61fe1cde5b40ba2f89e10e0045ba
Silvan Mosberger <contact@infinisil.com>
2020-08-11 03:34:05 +0200

    Set stopIfChanged on sshd@ unit instead for startWhenNeeded

--- modules/options.nix
+++ modules/options.nix
@@ -3,7 +3,9 @@ let
   inherit (lib) types;
 
   extraConfig = { lib, ... }: {
-    systemd.services.sshd.stopIfChanged = lib.mkForce true;
+    # By default the sshd service doesn't stop when changed so you don't lose connection to it when misconfigured
+    # But in Nixus we want to detect a misconfiguration since we can rollback in that case
+    systemd.services.${if config.services.openssh.startWhenNeeded then "sshd@" else "sshd"}.stopIfChanged = lib.mkForce true;
   };
 
   pkgsModule = nixpkgs: { lib, config, ... }: {
