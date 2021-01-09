a2233c5eb17ca8e5a00a510d6a97824c5cc30325
Silvan Mosberger <contact@infinisil.com>
2021-01-09 08:35:27 +0100

    Update nixpkgs to latest nixos-unstable

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/tarball/3320a06049fc259e87a2bd98f4cd42f15f746b96";
-  sha256 = "1g5l186d5xh187vdcpfsz1ff8s749949c1pclvzfkylpar09ldkl";
+  url = "https://github.com/NixOS/nixpkgs/tarball/f211631c1cb3e94828c7650b5d12c1e5a89e0e16";
+  sha256 = "0r085j42991qcbzx4l0hnwlsxw016y4b7r821s4qxvqnvwr9lxar";
 }
