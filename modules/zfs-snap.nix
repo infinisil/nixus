931bfd0de2e45ecd0e18c55cdc12154cf8afce35
eyjhb <eyjhbb@gmail.com>
2022-01-14 13:48:33 +0100

    added retention period

--- modules/zfs-snap.nix
+++ modules/zfs-snap.nix
@@ -12,19 +12,31 @@ let
     # run snaps for all the defined pools
   '' + (lib.concatMapStringsSep "\n" (x: "echo Taking snapshot for ${x} && zfs snapshot ${x}@$tag") pools));
 
-  mkScriptCleanup = prefix: pools: retention: nixus.pkgs.writeShellScript "deploy-zfs-snap-cleanup" (lib.concatMapStringsSep "\n" (x: "echo Deleting snapshots for ${x} && zfs list -t snapshot ${x} -o name | tail -n +2 | grep -i '${prefix}' | sort | head -n -${toString retention} | xargs -L 1 --no-run-if-empty zfs destroy") pools);
+  mkScriptCleanup = prefix: pools: retention: nixus.pkgs.writeShellScript "deploy-zfs-snap-cleanup" (lib.concatMapStringsSep "\n" (x: ''
+    echo Deleting snapshots for ${x} && \
+    zfs list -t snapshot -o name ${x} | \
+    tail -n +2 | \
+    grep '${prefix}' | \
+    sort | \
+    head -n -${toString retention} | \
+    xargs -L 1 --no-run-if-empty zfs destroy
+  '') pools);
 
   nodeOpts = {
     options = {
       pools = lib.mkOption {
         type = types.listOf types.str;
         default = [];
-        description = "List of pools to make snapshots for, ie. rpool/safe/persistent";
+        description = "List of pools to make snapshots for";
       };
       retention = lib.mkOption {
         type = types.int;
         default = 30;
-        description = "How many previous ZFS snapshots to keep";
+        description = ''
+          How many previous snapshots to keep.
+          If set to zero, then all snapshots will be deleted upon successful switch.
+          If less than zero, the cleanup script will never run.
+        '';
       };
     };
   };
@@ -50,33 +62,51 @@ in {
       type = types.submodule baseOpts;
       default = {};
       description = ''
-        Used to make ZFS snapshots on host, before deploying and switching to configuration.
-        Cleanup is also done, where a specified number of previous snapshot can be keept, before they are removed.
+        Used to make ZFS snapshots on nodes, before deploying and switching to configuration.
+        Cleanup is done after a successfull switch, where a specified number of previous snapshot will be keept using the retention number.
       '';
+      example = {
+        nodes = {
+          srtoffee = {
+            pools = [
+              "rpool/safe/persistent"
+              "rpool/safe/user"
+            ];
+            retention = 30;
+          };
+        };
+        prefix = "nixus-snapshots-";
+      };
     };
   };
 
   config.nodes = let
     nnodes = lib.mapAttrs (_: v: {
-      deployScriptPhases.zfs-snap-create = let
-        script = mkScriptCreate config.zfs-snap.prefix v.pools;
-      in lib.dag.entryBefore ["switch"] ''
-        echo Copying ZFS snapshotting create script
-        nix-copy-closure --to "$HOST" ${script}
-        echo Taking ZFS snapshots
-        ssh "$HOST" ${script}
-        echo Finished taking snapshots
-      '';
+      deployScriptPhases = let
+        scriptCreate = mkScriptCreate config.zfs-snap.prefix v.pools;
+        scriptCleanup = mkScriptCleanup config.zfs-snap.prefix v.pools v.retention;
+        fullScriptCreate = ''
+          echo Copying ZFS snapshotting create script
+          nix-copy-closure --to "$HOST" ${scriptCreate}
+          echo Taking ZFS snapshots
+          ssh "$HOST" ${scriptCreate}
+          echo Finished taking snapshots
+        '';
 
-      deployScriptPhases.zfs-snap-cleanup = let
-        script = mkScriptCleanup config.zfs-snap.prefix v.pools v.retention;
-      in lib.dag.entryAfter ["switch"] ''
-        echo Copying ZFS snapshotting cleanup script
-        nix-copy-closure --to "$HOST" ${script}
-        echo Cleaning ZFS snapshots
-        ssh "$HOST" ${script}
-        echo Finished cleaning snapshots
-      '';
+        fullScriptCleanup = ''
+          echo Copying ZFS snapshotting cleanup script
+          nix-copy-closure --to "$HOST" ${scriptCleanup}
+          echo Cleaning ZFS snapshots
+          ssh "$HOST" ${scriptCleanup}
+          echo Finished cleaning snapshots
+        '';
+
+      in {
+        zfs-snap-create = lib.dag.entryBefore ["switch"] fullScriptCreate;
+      } // (if (v.retention >= 0)
+            then { zfs-snap-cleanup = lib.dag.entryAfter ["switch"] fullScriptCleanup; }
+            else {}
+      );
     }) config.zfs-snap.nodes;
   in nnodes;
 }
