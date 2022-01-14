9acda44b6535b32c050b941e0806dcf3172ba365
eyjhb <eyjhbb@gmail.com>
2022-01-14 13:59:22 +0100

    added description, changed minor things

--- modules/groups.nix
+++ modules/groups.nix
@@ -3,8 +3,7 @@
 let
   inherit (lib) types;
 
-  # we need to reference the global configuration
-  # in our submodule.
+  # we need to reference the global configuration in our submodule.
   gconfig = config;
 
   groupOpts = { config, ... }: {
@@ -21,8 +20,7 @@ let
     };
 
     config.deployScript = let
-      mkDeployScript = members: nixus.pkgs.writeScript "deploy" ''
-        #!${nixus.pkgs.runtimeShell}
+      mkDeployScript = members: nixus.pkgs.writeShellScript "deploy" ''
         ${lib.concatMapStrings (nodeName: lib.optionalString gconfig.nodes.${nodeName}.enabled ''
 
           ${gconfig.nodes.${nodeName}.deployScript} &
@@ -36,6 +34,22 @@ in {
     groups = lib.mkOption {
       type = types.attrsOf (types.submodule groupOpts);
       default = {};
+      description = ''
+        Allows creating groups of nodes for deploying them together, instead of deploying all nodes.
+        Building the script for deploying can be done like `nix-build -A config.groups.home.deployScript`
+     '';
+      example = {
+        servers = [
+          "vps-web"
+          "vps-web2"
+          "vps-db"
+        ];
+        home = [
+          "eos"
+          "server1"
+          "router"
+        ];
+      };
     };
   };
 }
