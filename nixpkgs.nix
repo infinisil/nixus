a820fc1fb935d8f22f3dbcede15497650592c85e
eyJhb <eyjhbb@gmail.com>
2023-05-25 13:45:13 +0200

    bumped nixpkgs version

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/tarball/545c7a31e5dedea4a6d372712a18e00ce097d462";
-  sha256 = "sha256:1dbsi2ccq8x0hyl8n0hisigj8q19amvj9irzfbgy4b3szb6x2y6l";
+  url = "https://github.com/NixOS/nixpkgs/tarball/f91ee3065de91a3531329a674a45ddcb3467a650";
+  sha256 = "sha256:1jgcgllp8pxjz3q6r2yswsvlg96wlcnvdpgi71k7inibffqhq0z9";
 }
