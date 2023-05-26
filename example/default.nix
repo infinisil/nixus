ef1124ff5815bfd31f7dd6dc4b02e4316ad5911c
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-05-26 19:25:25 +0200

    Fix example

--- example/default.nix
+++ example/default.nix
@@ -1,26 +1,28 @@
 import ../. {} ({ config, ... }: {
 
-  defaults = { name, ... }: {
-    configuration = { lib, ... }: {
+  defaults = { lib, name, ... }: {
+    configuration = {
       networking.hostName = lib.mkDefault name;
     };
 
     # Which nixpkgs version we want to use for this node
-    nixpkgs = fetchTarball {
+    nixpkgs = lib.mkDefault (fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/tarball/81cef6b70fb5d5cdba5a0fef3f714c2dadaf0d6d";
       sha256 = "1mj9psy1hfy3fbalwkdlyw3jmc97sl9g3xj1xh8dmhl68g0pfjin";
-    };
+    });
   };
 
   nodes.foo = { lib, config, ... }: {
     # How to reach this node
-    host = "root@138.68.83.114";
+    host = "root@172.20.83.114";
 
     # What configuration it should have
     configuration = ./configuration.nix;
   };
 
   nodes.legacyNixpkgs = { lib, config, ... }: {
+    # How to reach this node
+    host = "root@172.20.83.115";
 
     nixpkgs = fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/tarball/38431cf21c59a84c0ddedccc0cd66540a550ec26";
