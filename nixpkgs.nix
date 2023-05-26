d8c3e403978da7b11a5dea1d9e8fd4f918668fdd
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-05-26 19:29:36 +0200

    Remove old nixpkgs.nix file

--- nixpkgs.nix
+++
@@ -1,4 +0,0 @@
-fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/tarball/f91ee3065de91a3531329a674a45ddcb3467a650";
-  sha256 = "sha256:1jgcgllp8pxjz3q6r2yswsvlg96wlcnvdpgi71k7inibffqhq0z9";
-}
