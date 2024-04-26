e5dc65b30c2e94ff37080057f0248c8e255e4746
Silvan Mosberger <silvan.mosberger@tweag.io>
2024-04-26 23:15:46 +0200

    Update to latest Nixpkgs unstable

--- example/deploy.nix
+++ example/deploy.nix
@@ -19,8 +19,8 @@
 
       # Which nixpkgs version we want to use for this node
       nixpkgs = lib.mkDefault (fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/tarball/81cef6b70fb5d5cdba5a0fef3f714c2dadaf0d6d";
-        sha256 = "1mj9psy1hfy3fbalwkdlyw3jmc97sl9g3xj1xh8dmhl68g0pfjin";
+        url = "https://github.com/NixOS/nixpkgs/tarball/e12483116b3b51a185a33a272bf351e357ba9a99";
+        sha256 = "1ili85f34m2ihbcnj1jm7qrsz5ql405zhp07qkngn0gn2c6lyx8l";
       });
     };
 
@@ -37,8 +37,8 @@
       host = "root@172.20.83.115";
 
       nixpkgs = fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/tarball/38431cf21c59a84c0ddedccc0cd66540a550ec26";
-        sha256 = "0bi5lkq2a34pij00axsa0l0j43y8688mf41p51b6zyfdzgjgsc42";
+        url = "https://github.com/NixOS/nixpkgs/tarball/e12483116b3b51a185a33a272bf351e357ba9a99";
+        sha256 = "1ili85f34m2ihbcnj1jm7qrsz5ql405zhp07qkngn0gn2c6lyx8l";
       };
 
       # What configuration it should have
